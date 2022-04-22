e6efca622388be3ff433f975990d6713908ed7f3
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-04-22 11:07:35 +0100

    cleaned scrapper project

--- Web.Scrapper/Data/MyDbContext.cs
+++ Web.Scrapper/Data/MyDbContext.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Scrapper.Models;
+
+namespace Web.Scrapper.Data;
+
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions<MyDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<ArticleMatrix>? ArticleMatrices { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
